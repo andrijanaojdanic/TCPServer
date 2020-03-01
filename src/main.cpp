acde02d6c58c5023bf78df87a6a014f360455ff3
Andrijana Ojdanic <andrijana.ojdanic@gmail.com>
2020-03-01 16:49:14 +0100

    First draft version. No oop principles yet.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,89 @@
 #include <iostream>
+#include <cerrno>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+constexpr static int DEFAULT_PROTOCOL = 0;
+constexpr static int RECV_BUF_SZ = 1024;
 
 int main()
 {
-	std::cout << "Hello!" << std::endl;
+    int serverSocketFd = 0;
+    struct sockaddr_in serverAddress;
+        serverAddress.sin_family = AF_INET;
+        serverAddress.sin_port   = htons(5000), // htons = Host To Network Short, makes sure that bytes are stored in network data order
+                                                // Short because it works only on 16b integers
+        serverAddress.sin_addr.s_addr = htonl(INADDR_ANY); // htonl = Host To Network Long, works with 32b integers
+
+    struct sockaddr clientAddress;
+    socklen_t clientAddressLen = sizeof(clientAddress);
+    int msgSize = 0;    // in number of bytes
+    int clientSocketFd = 0;
+
+    char recv_buffer[RECV_BUF_SZ];
+
+	// Create socket
+    serverSocketFd = socket(AF_INET, SOCK_STREAM, DEFAULT_PROTOCOL);    
+    if (serverSocketFd < 0)
+    { 
+        perror("Error creating socket");
+    }
+    else
+    {
+        // Bind socet to inet addr
+        if (bind(serverSocketFd, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) != 0)
+        {
+            perror("Error binding socket");
+        }
+        else
+        {
+            // Put socket in server mode - listen to connections
+            listen(serverSocketFd,1);
+
+            std::cout << "Waiting for connection..." << std::endl;
+
+            while (true)
+            {
+                // Accept connection
+                clientSocketFd = accept(serverSocketFd, &clientAddress, &clientAddressLen);
+                if (clientSocketFd < 0)
+                {
+                    perror("Error accepting connection");
+                    break;
+                }
+                else
+                {
+                    while (true)
+                    {
+                        std::cout << "Connection from " << clientAddress.sa_data << std::endl;
+                        // Receive end echo messages
+                        msgSize = recv(clientSocketFd, recv_buffer, RECV_BUF_SZ, 0);
+                        if (msgSize > 0)
+                        {
+                            std::cout << recv_buffer << std::endl;
+                            if (!send(clientSocketFd, recv_buffer, msgSize, 0))
+                            {
+                                perror("Error sending message");
+                                break;
+                            }
+                        }
+                        else 
+                        {
+                            std::cout << "Connection with " << clientAddress.sa_data << "terminated." << std::endl;
+                            close(clientSocketFd);
+                            break;
+                        }
+                    }
+                    
+                }
+            }
+        }
+    }
+
+   
 
 	return 0;
 }
+
